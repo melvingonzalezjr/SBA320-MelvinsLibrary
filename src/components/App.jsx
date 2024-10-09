dc40bddb1f7a3d61babcac87b849506d4992e3a9
melvingonzalezjr <mgonzalez713@gmail.com>
2024-10-09 08:41:31 -0500

    Created first Draft of Main Page with Navigation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,39 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './Navbar';
-import CategoryPage from './CategoryPage';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./Navbar";
+import CategoryPage from "./CategoryPage";
 
-function App() {
+
+export default function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/favorites" element={<CategoryPage category="Favorites" />} />
-        <Route path="/reading-now" element={<CategoryPage category="Reading Now" />} />
-        <Route path="/to-read" element={<CategoryPage category="To Read" />} />
-        <Route path="/have-read" element={<CategoryPage category="Have Read" />} />
-      </Routes>
+      <div>
+        <header>
+          <h1>Welcome to Melvin's Library</h1> 
+        </header>
+        <Navbar /> 
+        <main>
+          <Routes>
+            <Route path="/" element={<CategoryPage />} />
+            <Route
+              path="/favorites"
+              element={<CategoryPage category="Favorites" />}
+            />
+            <Route
+              path="/reading-now"
+              element={<CategoryPage category="Reading Now" />}
+            />
+            <Route
+              path="/to-read"
+              element={<CategoryPage category="To Read" />}
+            />
+            <Route
+              path="/have-read"
+              element={<CategoryPage category="Have Read" />}
+            />
+          </Routes>
+        </main>
+      </div>
     </Router>
   );
 }
-
-export default App;
